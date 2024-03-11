e7861cf8d9721ba930e7d37bbe4f37a04dad9673
YeeChuen <tychuen98@gmail.com>
2024-03-11 12:47:23 -0500

    Initial Commit

---
+++ Recipe book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recipe book
+//
+//  Created by Yee chuen Teoh on 3/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recipe book/Recipe_bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_bookApp.swift
+//  Recipe book
+//
+//  Created by Yee chuen Teoh on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
